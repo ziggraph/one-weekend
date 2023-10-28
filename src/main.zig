eb549062ee3bda8325eff5b4ca300cabb705321e
Celio Grand <celiogrand@outlook.com>
2023-10-29 04:10:07 +0800

    2.1. The PPM Image Format

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,31 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const image_width = 256;
+    const image_height = 256;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout.print("P3\n{d} {d}\n255\n", .{ image_width, image_height });
 
-    try bw.flush(); // don't forget to flush!
-}
+    for (0..image_height) |j| {
+        for (0..image_width) |i| {
+            const fi: f32 = @floatFromInt(i);
+            const fj: f32 = @floatFromInt(j);
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+            const r = fi / (image_width - 1);
+            const g = fj / (image_height - 1);
+            const b = 0;
+
+            const ir: i32 = @intFromFloat(255.999 * r);
+            const ig: i32 = @intFromFloat(255.999 * g);
+            const ib: i32 = @intFromFloat(255.999 * b);
+
+            try stdout.print("{d} {d} {d}\n", .{ ir, ig, ib });
+        }
+    }
+
+    try bw.flush();
 }
